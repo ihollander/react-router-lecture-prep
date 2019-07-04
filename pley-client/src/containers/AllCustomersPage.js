6c1b86a4992dc87e70bf4766ad4257675343da61
ihollander <ianholla@gmail.com>
2019-07-04 18:09:37 -0400

    final tweaks;

--- pley-client/src/containers/AllCustomersPage.js
+++ pley-client/src/containers/AllCustomersPage.js
@@ -3,30 +3,71 @@ import CustomerContainer from "./CustomerContainer";
 import CustomerPage from "./CustomerPage";
 
 class AllCustomersPage extends React.Component {
+  state = {
+    customers: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/customers")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          customers: data,
+          loading: false
+        });
+      });
+  }
+
+  addReview = review => {
+    const updatedCustomers = this.state.customers.map(customer => {
+      if (customer.id === review.customer_id) {
+        return {
+          ...customer,
+          reviews: [...customer.reviews, review]
+        };
+      } else {
+        return customer;
+      }
+    });
+
+    this.setState({ customers: updatedCustomers });
+  };
+
   getSearchedCustomers() {
     if (this.props.search !== "") {
-      return this.props.customers.filter(customer => {
+      return this.state.customers.filter(customer => {
         return customer.name
           .toLowerCase()
           .includes(this.props.search.toLowerCase());
       });
     } else {
-      return this.props.customers;
+      return this.state.customers;
     }
   }
 
   getSelectedCustomer() {
-    return this.props.customers.find(
+    return this.state.customers.find(
       customer => customer.id === this.props.currentCustomerId
     );
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <img
+          alt="loading..."
+          className="loader"
+          src="https://www.macupdate.com/images/icons256/54019.png"
+        />
+      );
+    }
+
     if (this.props.currentCustomerId) {
       return (
         <CustomerPage
           customer={this.getSelectedCustomer()}
-          addReview={this.props.addReview}
+          addReview={this.addReview}
         />
       );
     } else {
